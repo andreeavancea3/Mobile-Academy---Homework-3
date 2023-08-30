fa45c2bb666c15eab09734e2d9497921769b5db9
Vancea Andreea <andreea_vancea28@yahoo.com>
2023-08-30 19:27:17 +0300

    Endpoint for customers

--- src/main/java/com/example/demoJPA/controller/CustomersController.java
+++ src/main/java/com/example/demoJPA/controller/CustomersController.java
@@ -3,6 +3,7 @@ package com.example.demoJPA.controller;
 import com.example.demoJPA.dto.PostalDetailsUserDTO;
 import com.example.demoJPA.model.Customers;
 import com.example.demoJPA.model.OrderDetails;
+import com.example.demoJPA.model.Orders;
 import com.example.demoJPA.service.CustomersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -36,6 +37,13 @@ public class CustomersController {
         c.setPostalCode("123456");
         c.setCountry("Romania");
         customersService.createCustomer(c);
+    }
+
+    //create an endpoint that can display all orders belonging to a customer
+    @GetMapping("/{customerId}/orders")
+    public ResponseEntity<List<Orders>> getAllOrdersForCustomer(@PathVariable Integer customerId) {
+        List<Orders> orders = customersService.getAllOrdersForCustomer(customerId);
+        return ResponseEntity.ok(orders);
     }
 
     // create a DTO to return only the address, phone and city of the customer,
--- src/main/java/com/example/demoJPA/service/CustomersService.java
+++ src/main/java/com/example/demoJPA/service/CustomersService.java
@@ -2,10 +2,12 @@ package com.example.demoJPA.service;
 
 import com.example.demoJPA.dto.PostalDetailsUserDTO;
 import com.example.demoJPA.model.Customers;
+import com.example.demoJPA.model.Orders;
 import com.example.demoJPA.repository.CustomersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
@@ -23,4 +25,11 @@ public class CustomersService {
     public Customers findCustomerByUsername(String username) {
         return customersRepository.findByUsername(username);
     }
+
+    public List<Orders> getAllOrdersForCustomer(Integer customerId) {
+        Customers customer = customersRepository.findById(customerId)
+                .orElseThrow(() -> new EntityNotFoundException("Customer not found"));
+
+        return customer.getOrders();
+}
 }
